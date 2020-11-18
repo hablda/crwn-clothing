78001a4c72e807442a94e3b87e8ffb46a185ce0e
David Habl <habl@redblue.de>
2020-11-18 11:45:40 +0100

    feat: implemented firebase utils, SignUp component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./Pages/ShopPage/ShopPage";
 import Header from "./Components/Header/Header";
 import SignInPage from "./Pages/SignInPage/SignInPage";
-import { auth } from "./Firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./Firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -19,8 +19,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({
+        currentUser: userAuth,
+      });
     });
   }
 
--- src/Pages/SignInPage/SignInPage.jsx
+++ src/Pages/SignInPage/SignInPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./SignInPage.styles.scss";
 import SignIn from "../../Components/SignIn/SignIn";
+import SignUp from "../../Components/SignUp/SignUp";
 
 const SignInPage = () => {
   return (
-    <div>
+    <div className="sign-in-and-sign-up">
       <SignIn></SignIn>
+      <SignUp></SignUp>
     </div>
   );
 };
